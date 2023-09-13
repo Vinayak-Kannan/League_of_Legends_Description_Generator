20bc43062480440a3dbd55261513d30144ed140d
vinayakkannan <vk2364@columbia.edu>
2023-09-13 13:27:24 -0400

    Working mvp

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from "axios";
+import {useEffect} from "react";
+
+const getData = async () => {
+  const response = await axios.get('http://localhost:8000/ping')
+  console.log(response)
+}
+
 
 function App() {
+
+  useEffect(() => {
+    void getData()
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      Hello
     </div>
   );
 }
